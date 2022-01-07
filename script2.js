587659e0f239291ab15216c000e01e52e90e355d
angels-web <you@example.com>
2022-01-07 17:39:28 +0100

    start

---
+++ script2.js
@@ -0,0 +1,13 @@
+function hoverOnLeft(){
+  document.getElementById("img_right").class.add("active");
+}
+function hoverOffLeft(){
+  document.getElementById("img_right").class.remove("active");
+}
+function hoverOnRight(){
+  document.getElementById("icon_left").class.add("active");
+  }
+
+  function hoverOffRight(){
+    document.getElementById("icon_left").class.remove("active");
+    }

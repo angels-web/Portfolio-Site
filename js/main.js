1ff3a26bdc78b5dd4b53b2a2f7a550f99f912da0
angels-web <you@example.com>
2022-01-10 17:05:17 +0100

    new index site

--- js/main.js
+++ js/main.js
@@ -1,6 +1,28 @@
 
+/*HEY AROUND*/
 array = document.getElementById("hey-around-array");
 image = document.getElementById("hey-around-image");
+
+
+array.addEventListener("mouseleave", effectsOffHover);
+array.addEventListener("mouseenter", effectsOnHover);
+
+image.addEventListener("mouseleave", effectsOffHover);
+image.addEventListener("mouseenter", effectsOnHover);
+
+function effectsOnHover(e){
+  array.classList.add("button-hover");
+  image.classList.add("image-hover")
+}
+
+function effectsOffHover(e){
+  array.classList.remove("button-hover");
+  image.classList.remove("image-hover")
+}
+
+/* LOST*/
+array = document.getElementById("lost-array");
+image = document.getElementById("lost-image");
 
 
 array.addEventListener("mouseleave", effectsOffHover);
